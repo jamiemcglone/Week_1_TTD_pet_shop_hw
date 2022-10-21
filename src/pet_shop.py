064e5008654604b3615224ccab5b697fcd623b82
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-10-21 16:08:45 +0100

    Added 7 more functions.

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -11,3 +11,31 @@ def add_or_remove_cash(pet_shop, amount):
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
+def increase_pets_sold(pet_shop, amount):
+    pet_shop["admin"]["pets_sold"] += amount
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets_by_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_by_breed.append(pet["breed"])
+    return pets_by_breed
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    return None
+
+def remove_pet_by_name(pet_shop, name):
+    pet_to_remove = find_pet_by_name(pet_shop, name)
+    pet_shop["pets"].remove(pet_to_remove)
+
+def add_pet_to_stock(pet_shop, pet_to_add):
+    pet_shop["pets"].append(pet_to_add)
+
+def get_customer_cash(customer):
+    return customer["cash"]
