623bafde73a3b357c3144735f830f0ea535ce3df
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-10-21 14:06:45 +0100

    Completed first 4 functions.

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,13 @@
 # WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
